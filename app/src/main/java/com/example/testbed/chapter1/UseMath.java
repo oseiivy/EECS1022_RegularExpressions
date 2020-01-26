8a247f76d2db478878a79bdc09434a04f90659aa
Ivy Osei <oseiivy@my.yorku.ca>
2020-01-26 00:36:17 -0500

    TestBed commit

--- app/src/main/java/com/example/testbed/chapter1/UseMath.java
+++ app/src/main/java/com/example/testbed/chapter1/UseMath.java
@@ -2,5 +2,18 @@ package com.example.testbed.chapter1;
 
 public class UseMath
 {
-    result = Math.pow()
+    public static void main(String[] args)
+    {
+        double a = Math.pow(27, 0.3);
+        System.out.println(a);
+
+        double b = Math.pow(27, 0.3333);
+        System.out.println(b);
+
+        double c = Math.pow(27, 1/3);
+        System.out.println(c);
+
+        double d = Math.pow(27, 1.0/3.0);
+        System.out.println(b);
+    }
 }
