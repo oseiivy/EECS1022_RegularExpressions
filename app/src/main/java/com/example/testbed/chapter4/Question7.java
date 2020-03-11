b83b55cda1c5c49534229ca21949a992b59b6f0e
Ivy Osei <oseiivy@my.yorku.ca>
2020-03-10 22:33:00 -0400

    TestBed commit

---
+++ app/src/main/java/com/example/testbed/chapter4/Question7.java
@@ -0,0 +1,24 @@
+package com.example.testbed.chapter4;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Question7
+{
+    public static int totalCredit(String str)
+    {
+        int sum = 0;
+        String regex = "([E][E][C][S]|[e][e][c][s]|[C][S][E]|[c][s][e])\\d{4}\\.+(\\[)?(\\d)";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(str);
+        for (; matcher.find();)
+        {
+            sum = sum + Integer.parseInt(matcher.group(3));
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Question7.totalCredit("Course EECS1022..3 and CSE1020.[4] with CSE3241 or CSE6215...8"));
+    }
+}
